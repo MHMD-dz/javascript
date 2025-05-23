781e3f5861e0767634563893e7575583b4736b1c
mohamed <mhmdhayza@gmaile.com>
2025-05-23 22:43:09 +0100

    object

---
+++ object/script.js
@@ -0,0 +1,53 @@
+let car = {
+    name: 'mercides',
+    model: 2025 ,
+    color: 'red',
+    price: 20000,
+    isAvailable: true,
+
+    toString: function() {
+        console.log(`
+            Car Name: ${this.name} \n
+            Car Model: ${this.model} \n
+            Car Color: ${this.color} \n
+            Car Price: ${this.price} \n
+            Car is Available: ${this.isAvailable}`);
+    }
+}
+
+let user = {
+    // attributes
+    firstname: 'mohamed',
+    lastname: 'benzineb',
+    email: 'test@gmail.com',
+    age: 21,
+    skills: ['html', 'css', 'js' , 'c++' , 'java'],
+    activ: true,
+    phoneNumber:{
+        firstNumber: '1111',
+        secondNumber: '2222',
+        thirdNumber: '3333',
+                },
+    addres:{
+        algeria: "oran",
+        germany: "berlin",
+    },
+
+    // methods
+    isActiv:function() {
+        if(this.activ) {
+            console.log('hello usre');
+        }else{
+            console.log('goodbye user');
+        }
+    },
+    getAge: function() {
+        if (this.age >= 18) {
+            console.log('you are adult');
+        }else {
+            console.log('you are not adult');
+        }
+    },
+}
+
+console.log(user['phoneNumber']['firstNumber']);

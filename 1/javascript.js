c1744c0edd877c1f15818f69cd9b74f1ed3e6442
mohamed <mhmdhayza@gmaile.com>
2025-06-01 19:20:52 +0100

    cs

--- 1/javascript.js
+++ 1/javascript.js
@@ -11,3 +11,15 @@ console.log(Math.min(5, 10, 15));
 console.log(Math.round(11.5));
 console.log(Math.floor(11.7));
 console.log(Math.ceil(11.1));
+
+// 
+let i = 0 ;
+function hi() {
+    console.log(`I love her a ${i++} times`);
+    if (i == 15) {
+        clearInterval(y);
+        return console.log("I love her more than anything in the world");
+        
+    }
+}
+let y = setInterval(hi , 1000);
---
+++ screen/script.js
@@ -0,0 +1,14 @@
+// This script logs the screen dimensions to the console
+console.log( screen.width );
+console.log( screen.height );
+
+// The available width and height of the screen
+console.log( screen.availWidth );
+console.log( screen.availHeight );
+
+// The color depth of the screen
+console.log( screen.colorDepth );
+// The pixel depth of the screen
+console.log( screen.pixelDepth );
+// The orientation of the screen
+console.log( screen.orientation.type );
